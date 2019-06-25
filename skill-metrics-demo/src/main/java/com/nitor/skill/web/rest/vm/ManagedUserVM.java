2f550ece3a22f1bd7d6e09cbdd312bdfb3ab5561
pramoddhone <pramoddhone@yahoo.com>
2019-06-25 14:28:20 +0530

    Initial application generated by JHipster-6.1.0

---
+++ skill-metrics-demo/src/main/java/com/nitor/skill/web/rest/vm/ManagedUserVM.java
@@ -0,0 +1,19 @@
+package com.nitor.skill.web.rest.vm;
+
+import com.nitor.skill.service.dto.UserDTO;
+
+/**
+ * View Model extending the UserDTO, which is meant to be used in the user management UI.
+ */
+public class ManagedUserVM extends UserDTO {
+
+    public ManagedUserVM() {
+        // Empty constructor needed for Jackson.
+    }
+
+    @Override
+    public String toString() {
+        return "ManagedUserVM{" +
+            "} " + super.toString();
+    }
+}
